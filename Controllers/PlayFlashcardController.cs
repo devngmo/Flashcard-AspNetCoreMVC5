17836b544d658a1844272abc90b493b04d1eb4ae
Tran Manh Linh <devngmo@gmail.com>
2021-12-15 18:01:41 +0700

    import project

---
+++ Controllers/PlayFlashcardController.cs
@@ -0,0 +1,38 @@
+﻿using AspNetCoreMVC5.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC5.Controllers
+{
+    public class PlayFlashcardController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public PlayFlashcardController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.BASE_URL = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
